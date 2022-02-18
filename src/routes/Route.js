a0c0a093029197ce50f3a05034a209a58632e1ab
Ezequiel Nascimento Campos <ezequiel.campos@fgv.br>
2022-02-18 19:35:32 -0300

    inicio

---
+++ src/routes/Route.js
@@ -0,0 +1,2 @@
+import React, {useContext} from 'react';
+import {Route as RectDOMRoute, useHistory} from 'react-router-dom';
---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import {BrowserRouter,Switch} from "react-router-dom"
+
+import Route from './Route';
